8e9c06857acea44d037b1f5ea8c302ce16cb4d06
APCS2nd <APCS2nd@MHS-LT9-07>
2019-08-21 12:16:55 -0700

    Complete Hello World Project Working On User Interface

---
+++ Hello World Project/src/HelloWorld.java
@@ -0,0 +1,12 @@
+// Lucas Jang 
+// Aug 19 2019
+// Hello World Project
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World!   From Lucas");
+	}
+
+}
+
