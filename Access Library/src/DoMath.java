48136f4fec503583b950a8e9ca1bbaf56d2811e9
APCS2nd <APCS2nd@MHS-LT9-07>
2019-09-04 12:03:10 -0700

    Calc Library Commit

---
+++ Access Library/src/DoMath.java
@@ -0,0 +1,13 @@
+// Lucas Jang
+// August 29, 2019
+// Period 4
+// Call math methods from Calculate Library
+// Client code or runner code
+
+public class DoMath {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,10 @@
+// Lucas Jang
+// August 29, 2019
+// Period 4
+// Group of methods that perform math calculations aka library
+
+public class Calculate {
+	public static void square() {
+		return
+	}
+}
